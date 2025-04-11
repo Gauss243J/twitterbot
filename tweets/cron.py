c20b08108ec7b9e843ff143c189637410ead0b4e
Gauss243J <78795548+Gauss243J@users.noreply.github.com>
2025-04-11 19:49:18 +0200

    Update cron.py

--- tweets/cron.py
+++ tweets/cron.py
@@ -1,1 +1,19 @@
+from django_cron import CronJobBase, Schedule
+from django.utils import timezone
+import requests
+
+class TweetScrapingJob(CronJobBase):
+    schedule = Schedule(run_every_mins=2880)  # 2880 minutes = 2 jours
+    code = 'myapp.tweet_scraping_job'
+
+    def do(self):
+        url = "https://twitterbot-jk9g.onrender.com/tweets/process_tweets/"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"{timezone.now()}: Scraping des tweets terminé avec succès!")
+            else:
+                print(f"{timezone.now()}: Erreur lors du scraping des tweets.")
+        except requests.exceptions.RequestException as e:
+            print(f"{timezone.now()}: Erreur de connexion: {e}")
 
