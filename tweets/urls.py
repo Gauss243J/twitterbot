fa0184f632edf2a9f1a9c04c8b7c32c618b942b5
Gauss243J <78795548+Gauss243J@users.noreply.github.com>
2025-04-11 19:50:19 +0200

    Update urls.py

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import process_tweets, twitter_callback
+from .views import process_tweets, twitter_callback, start_tweet_scraping
 
 
 urlpatterns = [
     path('process_tweets/', process_tweets, name='process_tweets'),
     path('twitter/callback/', twitter_callback, name='twitter_callback'),
+    path('scraping/', start_tweet_scraping, name='scraping'),
 ]
 
