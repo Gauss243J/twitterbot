e43d79d5f8f98b03bc188f15b78af0700bea65b7
Gauss243J <78795548+Gauss243J@users.noreply.github.com>
2025-04-11 18:42:06 +0200

    Update views.py

--- tweets/views.py
+++ tweets/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 # View to process tweets and repost them with modifications
 def process_tweets(request):
-    usernames = ['realfrance_fr', 'DivineProverbs']  # List of Twitter usernames
+    usernames = ['Mindset_FR', 'VibhorChandel','AlexSofamous', 'DivineProverbs']  # List of Twitter usernames
     for username in usernames:
         # Get the most recent tweets for the username
         tweets = get_tweets_from_user(username)
